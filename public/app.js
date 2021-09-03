802729345b6e2996e1a4eef53b868985fc370fda
Adwaita Raj Modak <adwaitaraj@gmail.com>
2021-09-04 01:30:34 +0530

    day_2

--- public/app.js
+++ public/app.js
@@ -44,7 +44,7 @@
 
 
 
-var events = require('events');
+// var events = require('events');
 
 // var myEmitter = new events.EventEmitter();
 
@@ -57,14 +57,78 @@ var events = require('events');
 // myEmitter.emit('someEvent', 'the event was emitted');
 
 
+// var events = require('events');
 
-var util = require('util');
-const { threadId } = require('worker_threads');
-
-
-var Person = function (name) {
-    this.name = name;
-}
+// var util = require('util');
 
 
-util.inherits(Person, events.EventEmitter)
+// var Person = function (name) {
+//     this.name = name;
+// }
+
+
+// util.inherits(Person, events.EventEmitter);
+// var james = new Person('james');
+// var pappu = new Person('pappu');
+// var harry = new Person('harry');
+
+// var people = [james, pappu, harry];
+
+// people.forEach(function (person) {
+//     person.on('speak', function (msg) {
+//         console.log(person.name + ' said ' + msg);
+
+//     })
+// })
+
+
+// james.emit('speak', 'wassup boi')
+
+///FILE
+
+// var fs = require('fs');
+
+// fs.readFile('readme.txt', 'utf8', function (err, data) {
+//     // console.log(data);
+
+//     fs.writeFileSync('writeMe.txt', data);
+
+
+// });
+
+// fs.unlink('readme.txt', function (err) { });
+
+// console.log('heypppppppppppppppppppppp');
+// fs.writeFileSync('write.txt', text);
+
+
+// console.log(work);
+
+// var fs = require('fs')
+
+// fs.mkdirSync('stuff');
+// fs.mkdir('stuff', function () {
+//     fs.readFile('write.txt', 'utf8', function (err, data) {
+//         fs.writeFile('./stuff/readme.txt', data, function () { })
+//     })
+// });
+// fs.rmdir('stuff', function () { })
+
+
+
+
+var http = require('http');
+
+
+var server = http.createServer(function (req, res) {
+
+    console.log('Request made ' + req.url);
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('HI raj')
+});
+
+
+
+server.listen(3000, '127.0.0.1')
+
+console.log('Listended to  port 3K');
