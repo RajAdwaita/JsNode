1b200a62710ee9fc78f911acbb7e784b92211ef1
Adwaita Raj Modak <adwaitaraj@gmail.com>
2021-09-06 02:09:22 +0530

    day3 FINAL

--- public/app.js
+++ public/app.js
@@ -224,6 +224,8 @@ var express = require('express');
 
 var app = express();
 
+app.set('view engine', 'ejs');
+
 
 app.get('/', function (req, res) {
     // res.send("Hello friends")
@@ -241,7 +243,10 @@ app.get('/contact', function (req, res) {
 
 
 app.get('/profile/:id', function (req, res) {
-    res.send('You requested to see a profile with the Id of ' + req.params.id);
+
+    var data = { age: 29, job: 'deveops', games: ['nfs', 'fifa', 'pes'] };
+    // res.send('You requested to see a profile with the Id of ' + req.params.id);
+    res.render('profile', { person: req.params.id, data: data });
 
 })
 
