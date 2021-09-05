a70139a41d02eaf0c3680320e293ae72eb756c44
Adwaita Raj Modak <adwaitaraj@gmail.com>
2021-09-06 00:13:19 +0530

    3rd edit

--- public/app.js
+++ public/app.js
@@ -120,11 +120,91 @@
 var http = require('http');
 
 
+// var server = http.createServer(function (req, res) {
+
+//     console.log('Request made ' + req.url);
+//     res.writeHead(200, { 'Content-Type': 'text/plain' });
+//     res.end('HI raj')
+// });
+
+
+
+// server.listen(3000, '127.0.0.1')
+
+// console.log('Listended to  port 3K');
+
+
+
+var fs = require('fs');
+const { stringify } = require('querystring');
+
+var myReadStream = fs.createReadStream(__dirname + '/readme.txt', 'utf-8');
+
+var myWriteStream = fs.createWriteStream(__dirname + '/writeMe.txt');
+// myReadStream.on('data', function (chunk) {
+//     console.log('new chunk received');
+
+//     myWriteStream.write(chunk);
+//     // console.log(chunk);
+
+
+
+// })
+
+myReadStream.pipe(myWriteStream);
+
+
 var server = http.createServer(function (req, res) {
 
     console.log('Request made ' + req.url);
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('HI raj')
+    // res.writeHead(200, { 'Content-Type': 'text/plain' });
+    // res.end('I need some more pasta')
+
+    if (req.url === '/home' || req.url === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        fs.createReadStream(__dirname + '/index.html').pipe(res);
+    }
+
+    else if (req.url === '/contact') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        fs.createReadStream(__dirname + '/contact.html').pipe(res);
+
+    }
+
+    else if (req.url === '/api/adwaita') {
+        var adwaita = [{
+            name: 'adwaita0',
+            age: 32,
+            status: 'sad'
+        },
+        {
+            name: 'Amvrin',
+            age: 15,
+            status: 'happy'
+        }];
+
+
+        res.writeHead(200, { 'Content-type': 'application/json' });
+        res.end(JSON.stringify(adwaita));
+    }
+
+    else {
+        res.writeHead(404, { 'Content-type': 'text/html' });
+        fs.createReadStream(__dirname + '/404.html').pipe(res);
+    }
+    // var myObj = {
+    //     name: 'Adwaita',
+    //     age: 19,
+    //     status: 'Happy'
+    // };
+    // res.end(JSON.stringify(myObj));
+
+
+
+    // var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf-8');
+    // myReadStream.pipe(res);
+
+
 });
 
 
@@ -132,3 +212,4 @@ var server = http.createServer(function (req, res) {
 server.listen(3000, '127.0.0.1')
 
 console.log('Listended to  port 3K');
+
